67ffa20bc5723ced1c42c4f8ca92084d13e2e213
SanjayKumaran D <sanjaykumaran6907@gmail.com>
2026-07-18 10:59:09 +0530

    folders fixed

--- w1jfsdips/student_management.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE student_management;
-USE student_management;
-CREATE TABLE students (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	student_name VARCHAR(100),
-	department VARCHAR(100),
-	email VARCHAR(100)
-);
